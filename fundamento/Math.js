752046eb7ebc01611d664f69771bcc4c31f1f62f
LeoSlavik <leozinhosantosoliveira@hotmail.com>
2021-01-25 18:31:14 -0300

    calculo de areas de geometricos

--- fundamento/Math.js
+++ fundamento/Math.js
@@ -2,4 +2,8 @@ const raio =5.6
 const area = Math.PI * Math.pow(raio , 2)
 
 console.log (area)
-console.log (typeof Math)
+console.log (typeof Math) 
+
+const base = 5 
+const altura =10 
+const areaDoTriangulo = base * altura / 2
