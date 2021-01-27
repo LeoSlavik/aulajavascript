b5720889ae6874296d891bb6500b6177560e5b89
LeoSlavik <leozinhosantosoliveira@hotmail.com>
2021-01-27 18:10:36 -0300

    Template Strings

---
+++ fundamento/templatestring.js
@@ -0,0 +1,31 @@
+const nome = 'alunos'
+const template = `
+
+sejam bem vindo 
+${nome}. 
+hoje estaremos apresentando o uso de Template Strings!!!`
+
+console.log (template)
+
+
+const justaposicao = 'lapis-borracha' // justaposições sempre sao a  concatenação de duas palavras 
+ const aglutinacao = 'la' + 'rracha'                                      // mas não se retira nenhuma letra  delas.
+ 
+ const JTemplates = `
+
+eu tenho um lapis com uma borracha na ponta
+e quero q seu nome seja derivado de uma justaposição.
+ele será : ${justaposicao}
+
+`
+console.log (JTemplates)
+const Atemplates = `
+
+mas  e se derivar de aglutinação será : ${aglutinacao} 
+
+`
+console.log (Atemplates)
+
+
+const up = texto => texto.toUpperCase()
+console.log (`3 Segundos e sera o ano novo ${up(' feliz ano novo !!!')}!`) 
