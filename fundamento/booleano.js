66b50236eb804ada6dbe9b925071df232cb098a7
LeoSlavik <leozinhosantosoliveira@hotmail.com>
2021-01-27 18:37:58 -0300

    booleanos ou lógicos

---
+++ fundamento/booleano.js
@@ -0,0 +1,31 @@
+let  mentira = false 
+let verdade = true
+
+console.log (mentira , verdade) 
+
+console.log ("os verdadeiros são :")
+
+console.log (!!3)
+console.log (!!-1)
+console.log (!!' ')
+console.log (!![])
+console.log (!!{})
+console.log (!!Infinity)
+
+
+console.log ("os falsos são :")
+console.log (!!0)
+console.log (!!'')
+console.log (!!null)
+console.log (!!NaN)
+console.log (!!undefined)
+console.log (!!(verdadeiro = false))
+
+
+
+console.log ("qual destes é o verdadeiro ?")
+
+console.log ('' || NaN || undefined || "o falso" )
+
+console.log ("qual destes pode ser o falso?") 
+console.log ('' || 0 || 'false' || null) 
