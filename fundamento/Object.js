1e390f65198a278b9e41743f8116b3fdadda7e9c
LeoSlavik <leozinhosantosoliveira@hotmail.com>
2021-01-28 19:37:16 -0300

    Utilizando Objetos (Ex : em Shopping)

---
+++ fundamento/Object.js
@@ -0,0 +1,31 @@
+const produto1 = {} 
+produto1.nome = 'Iphone X'
+produto1.preco = 7530.99
+
+const produto2 = {}
+produto2.nome = 'par de 6 camisetas da oakley'
+produto2.preco = 300.00
+
+const produto3 = {}
+produto3.nome = "frangos Perdigão (peito)"
+produto3.preco = 20.99
+
+const produto4 = {}
+produto4.nome = "Cerveja Lata 350ml BRAHMA"
+produto4.preco = 3.99
+
+var Inicio = `
+seja bem Vindo ao Shopping Ponto Frio 
+Aproveite nossos produtos de mais alta qualidade!!!
+
+a lista abaixo será listado os produtos a venda  
+
+↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓
+`
+console.log (Inicio)
+
+console.log (produto1)
+console.log (produto2)
+console.log (produto3)
+console.log (produto4)
+
