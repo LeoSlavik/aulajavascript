e5a52809597c8e39928f0fa89c7e4d1cab896bfe
LeoSlavik <leozinhosantosoliveira@hotmail.com>
2021-01-28 19:44:45 -0300

    Entendendo os valores Null e Undefined

---
+++ fundamento/NulleUndefined.js
@@ -0,0 +1,20 @@
+let valor 
+
+console.log (valor) // undefined pois n atribuiu nenhum valor a essa variavel
+
+valor = null 
+console.log (valor) // valor ausente  
+// se tentar usar comandos ex tostring num valor nulo vai dar error
+
+const produto ={} 
+console.log (produto.name) //como produto foi declarado mais seu nome nao, dá undefined  Ex: 
+produto.preco = "this product price is " ,null 
+console.log (produto.preco) 
+const lanche = {} 
+lanche.name = "X burguer"
+lanche.preco = 
+console.log (produto.preco) 
+// delete (produto.preco) tira o valor do objeto
+console.log (!!lanche.name)
+console.log (lanche)
+
