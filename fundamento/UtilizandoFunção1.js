53fc6261e42110a4c14a6527d7f9377f3f8b7aa7
LeoSlavik <leozinhosantosoliveira@hotmail.com>
2021-01-29 15:44:54 -0300

    funcoes aritméticas

---
+++ fundamento/UtilizandoFunção1.js
@@ -0,0 +1,49 @@
+
+//Soma
+
+function SomadorDeNumeros (a, b)  {
+
+console.log  (a + b)
+
+}
+
+SomadorDeNumeros (2 , 5) 
+
+//Multiplicação
+function Multiplicador (x , y) {
+
+
+console.log (x * y)
+
+}
+Multiplicador (10 , 5 )
+
+//Subtração 
+
+function SubtraçãoNumerica (C , F) {
+
+    console.log (C / F)
+
+}
+SubtraçãoNumerica (10 , 2)
+
+
+//divisão 
+
+function DivisaoDeInteiros (G , H)  {
+    
+    console.log (G / H) 
+}
+DivisaoDeInteiros (100 , 10)
+
+function exponenciacoes ( K , M) { 
+
+    console.log ( K ** M) 
+}
+exponenciacoes ( 10 , 2) 
+
+function Modulos (V , L) {
+
+    console.log (V % L)
+}
+Modulos (15 , 4)          //resto de divisões
