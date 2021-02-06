e29753071595a72013b89006fcada34735241bd8
LeoSlavik <leozinhosantosoliveira@hotmail.com>
2021-02-06 15:57:17 -0300

    Let em Loops

---
+++ fundamento/LetEmLoop.js
@@ -0,0 +1,19 @@
+
+// o let ira ignora o seu valor antigo e ira ser atualizado ao novo, que é o caso abaixo
+
+
+for (let i = 0; i <= 10; i ++) {
+
+    console.log (i)
+}
+
+
+
+
+
+for (let i = 2; i <=20; i++) {
+
+    console.log (i)
+}
+
+// este comando será executado apenas, o de cima será ignorado ou substituido.
