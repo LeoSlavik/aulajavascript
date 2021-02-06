36baa8305be6261add9af4f21d5f653513373483
LeoSlavik <leozinhosantosoliveira@hotmail.com>
2021-02-06 15:03:21 -0300

    Usando variaveis (2)

---
+++ fundamento/Variaveis2.js
@@ -0,0 +1,12 @@
+var numero = 5
+
+{
+
+var numero = 10
+console.log ("dentro" , numero)
+
+}
+console.log ("fora " , numero)
+
+/* evitar ao máximo o uso do escopo global do var para diminuir os erros
+*/
