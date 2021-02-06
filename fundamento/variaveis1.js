18a67a0b3e193c7a92621390e7560ec94dfc5225
LeoSlavik <leozinhosantosoliveira@hotmail.com>
2021-02-06 14:58:33 -0300

    Usando Variaveis (1)

---
+++ fundamento/variaveis1.js
@@ -0,0 +1,38 @@
+{
+    {
+   
+        {
+   
+            {
+   
+                {
+   
+                    {
+   
+                        {var sera = "será?????"
+console.log (sera) //o var pode ser transmitido mesmo fora do bloco onde foi declarado
+                                  }
+                                         } 
+                               } 
+                           } 
+                   }
+          } 
+      }
+      console.log (sera)
+
+
+      function teste () {
+
+        var numeroteste = 150
+    console.log (numeroteste)
+    } 
+/*1 escopo
+
+teste () 
+console.log(numeroteste)
+*/ 
+/* Chrome 
+
+quando declara uma variavel no chrome com o var  = ??? vc automaticamente ta criando um 
+escopo global na variavel, e pode ser acessado com o comando window.a
+*/
