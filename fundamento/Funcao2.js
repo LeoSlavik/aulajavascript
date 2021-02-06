3cb00d98a8eaf0af80040f2f1123ccbe1f6a43ba
LeoSlavik <leozinhosantosoliveira@hotmail.com>
2021-02-06 15:52:25 -0300

    var em loops

--- fundamento/Funcao2.js
+++ fundamento/Funcao2.js
@@ -28,3 +28,17 @@ console.log (divisao( 30 , 15))
 
 const Multiplicacao = (F , J) => F * J 
 console.log (Multiplicacao(10 , 30))
+
+// Revisão 1  problema da batata palha
+
+function Mercado () {
+
+ var Pnome = "Batata Palha 300g"
+ var ppreco = 7.50
+ var total = 10.00
+ var  troco = total - ppreco
+ console.log ("comprei um saco de batata palha por ", ppreco ," , dei ao moço uma nota de " ,total," reais ,ou seja, recebi",troco,"reais de troco" )
+
+}
+Mercado ()
+
---
+++ fundamento/VarEmLoop.js
@@ -0,0 +1,17 @@
+//repetição até 10 
+
+for (var i = 0; i <= 10 ; i++) {
+
+
+    console.log (i)
+}
+
+
+/*numeros pares 
+var y = 2
+for (var i = 0; i < 20 ; i + 2 ) {
+console.log (i)
+}    
+incompleto */
+
+ 
