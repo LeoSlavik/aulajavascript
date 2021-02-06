cb7ea0dc517f95e252ae4c59e22669005ab4017d
LeoSlavik <leozinhosantosoliveira@hotmail.com>
2021-02-06 15:07:25 -0300

    Usando let em variaveis

---
+++ fundamento/variaveiscomlet.js
@@ -0,0 +1,25 @@
+var numerico = 15
+ {
+   let numerico = 20
+   console.log ("dentro" , numerico)
+
+
+ }
+console.log ("fora" , numerico)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* nao esquecer que o let so tem efeito dentro de um bloco em especifico, ja o var é global
+*/
