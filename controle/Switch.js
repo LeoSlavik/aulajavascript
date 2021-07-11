513dfe26342b21efaa273b458fb1c09b4318920d
LeoSlavik <leozinhosantosoliveira@hotmail.com>
2021-07-11 14:20:58 -0300

    Aula Sobre Switch e Break

---
+++ controle/Switch.js
@@ -0,0 +1,37 @@
+const ImprimirResultado = function (nota) {
+
+   switch (Math.floor(nota)) {
+
+  case 10:
+  case 9 :
+
+  console.log (' quadro de honra')
+ break
+  case 8:
+  case 7 :
+    console.log ('quadro da elite')
+    break
+    case 6:
+   case 5:
+   console.log ('quadro da Média')
+   break
+   case 4:
+   case 3:
+   case 2:
+   case 1:        
+  console.log ('quadro inferior')
+
+  break
+  
+  default: 
+  console.log ('Nota Inválida')
+} 
+
+}
+
+ImprimirResultado(2)
+/*ImprimirResultado(5)
+ImprimirResultado(9)
+ImprimirResultado(1)
+ImprimirResultado(-6)
+ImprimirResultado(15)*/
