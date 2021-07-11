6395a5fcb97b8668a5f41d3bacb72bdd6782dfe7
LeoSlavik <leozinhosantosoliveira@hotmail.com>
2021-07-11 15:06:00 -0300

    Estrutura Do While no Js

--- controle/While.js
+++ controle/While.js
@@ -1,10 +1,10 @@
-function  MostrarNumeroAleatorio (min, max) {
+/*function  MostrarNumeroAleatorio (min, max) {
 
 const valor =  Math.random () * (max - min) + min
   return Math.floor (valor)
     
 }
-let opcao =0
+let opcao = 0
 
 while (opcao != -1) {
 
@@ -14,3 +14,20 @@ while (opcao != -1) {
 }
 
 console.log ('Até a proxima')
+
+*/
+function  GanhadorDaRifa (min, max){
+
+const valor2 = Math.random () * (max - min) + min
+
+ return Math.floor(valor2)
+}
+
+let NumeroVencedor = 0
+
+while (NumeroVencedor != 1) {
+
+    NumeroVencedor = GanhadorDaRifa (1 , 10) // num vo po cem se não o Pc trava 
+    console.log (`o ganhador da rifa foi ${NumeroVencedor}`)
+}
+console.log (' venha até avenida Brasil Para Receber Teu Premio')
