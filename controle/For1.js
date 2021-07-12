3f717013150773f1adcebd8f69fe80618ffa178d
LeoSlavik <leozinhosantosoliveira@hotmail.com>
2021-07-12 18:48:33 -0300

    usando o For no JavaScript

---
+++ controle/For1.js
@@ -0,0 +1,18 @@
+ let contador = 1
+ while (contador <= 10 ) {
+
+    console.log (`contador está no  ${contador}`)
+    contador ++
+ }
+
+ for (let i = 1; i <= 10; i ++) {
+   
+   console.log (`o contador é ${i}`)
+ }
+
+ const nota = [6.7 , 10.5 , 8.4 , 9.9]
+ for (i = 0 ; i < nota. length ; i ++) {
+
+    console.log (`o Array Selecionado é ${nota[i]}`)
+ }
+ // este comando é usado para Ver O Valor de Cada Array
