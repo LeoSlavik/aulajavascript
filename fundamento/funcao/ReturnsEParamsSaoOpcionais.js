7a6e635154020e9b3b7c3a9fdf316c5cfec94583
LeoSlavik <leozinhosantosoliveira@hotmail.com>
2021-07-31 12:09:46 -0300

    Returns e Params opcionais no JavaScript

---
+++ fundamento/funcao/ReturnsEParamsSaoOpcionais.js
@@ -0,0 +1,18 @@
+function area (largura , altura) {
+
+    const area = largura * altura
+if (altura > 20 ) {
+
+    console.log (`valor maximo ultrapassado ${area} metros quadrados `) 
+} else {
+
+    return area
+}
+
+}
+
+console.log (2 , 2)
+console.log (2)
+console.log (5, 9)
+console.log (2 , 4 , 1 ,10)
+console.log ()
